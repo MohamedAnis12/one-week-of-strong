77146ce05c8fdf0f9580f3728976024a26164691
anis <vidoyobegaming@gmail.com>
2026-03-04 05:41:55 +0200

    1st day create profile view

--- lib/views/profile/profile_view.dart
+++ lib/views/profile/profile_view.dart
@@ -6,7 +6,31 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 66,
+                  backgroundColor: Colors.black,
+                  child: CircleAvatar(
+                    radius: 64,
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.person, size: 24),
+                  ),
+                ),
+              ],
+            ),
+
+            Text("Name : Mohamed Anis",style: TextStyle(fontSize: 24,),)
+          ],
+        ),
+      ),
     );
   }
 }
